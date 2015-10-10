1abd8d3fb5c9e1cd9dad71244f7758bbb3890bc2
ajurczyk <aleksander.jurczyk@gmail.com>
2015-10-10 18:32:25 +0200

    0% filter added

--- cpp/Test/src/Test.cpp
+++ cpp/Test/src/Test.cpp
@@ -19,40 +19,39 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include "math.h"
 using namespace std;
 
-int read_adc();
+double convertAdcToPercent(int adc);
+double readThrottle(AnalogInput AIN, bool* correct);
+bool checkMeasurements(double measurements[], int size);
 
 int main() {
 	PwmPin pwmPin = PwmPin("P9_22");
 	pwmPin.SetPwmDuty(4000000);
 	AnalogInput Ain0 = AnalogInput("P9_39");
-	int adc;
 	double currentPower = 0;
 	double percent;
-	Mpu6050AccelGyro mpu6050 = Mpu6050AccelGyro(1, 0x68);
+	/*Mpu6050AccelGyro mpu6050 = Mpu6050AccelGyro(1, 0x68);
 
-	for(int i=0;i<100;i++){
-		double accX=mpu6050.readAccX();
-		cout<<i<< ".\tX = "<<accX<<"\tY = "<<mpu6050.readAccY()<<"\tZ = "<<mpu6050.readAccZ()<<endl;
-		usleep(100000);
-	}
-	/*while(1)
-	 {
-	 currentPower=percent;
-	 adc = Ain0.ReadAin();
-	 percent=(adc - 150)/16.5;
-	 if(percent<0)
-	 percent=0;
-
-	 cout<<"Read: "<<percent<<"% ";
-	 if(percent-currentPower>10)
-	 percent=currentPower+10;
-
-	 cout<<"Power set: "<<percent<<endl;
-	 pwmPin.SetPwmDuty(4000000-percent*10000);
+	 for(int i=0;i<100;i++){
+	 double accX=mpu6050.readAccX();
+	 cout<<i<< ".\tX = "<<accX<<"\tY = "<<mpu6050.readAccY()<<"\tZ = "<<mpu6050.readAccZ()<<endl;
 	 usleep(100000);
 	 }*/
+	while (1) {
+		currentPower = percent;
+		bool correct = true;
+		percent = readThrottle(Ain0, &correct);
+		if (!correct)
+			continue;
+		cout << "Read: " << percent << "% ";
+		if (percent - currentPower > 10)
+			percent = currentPower + 10;
+
+		cout << "Power set: " << percent << endl;
+		pwmPin.SetPwmDuty(4000000 - percent * 10000);
+	}
 
 	/*PwmPin pwmPin= PwmPin("P9_22");
 	 pwmPin.SetPwmDuty(4000000);
@@ -67,4 +66,34 @@ int main() {
 	 }*/
 	return 0;
 }
+double readThrottle(AnalogInput AIN, bool* correct) {
+	*correct = true;
+	const int probesAmount = 5;
+	double measurements[probesAmount];
+	for (int i = 0; i < probesAmount; i++) {
+		measurements[i] = convertAdcToPercent(AIN.ReadAin());
+		int sleepTimeMs = 10;
+		usleep(sleepTimeMs * 1000);
+	}
+	if (!checkMeasurements(measurements, probesAmount))
+		*correct = false;
 
+	return measurements[0];
+}
+double convertAdcToPercent(int adc) {
+	double percent = (adc - 150) / 16.5;
+	if (percent < 0)
+		percent = 0;
+	return percent;
+}
+bool checkMeasurements(double measurements[], int size) {
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			if (i == j)
+				continue;
+			if (abs(measurements[i] - measurements[j]) > 5)
+				return false;
+		}
+	}
+	return true;
+}
