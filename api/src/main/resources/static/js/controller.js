fa1b3c6dabe54b9c731cd28784928f29140a4ab4
AJurczyk <aleksander.jurczyk@gmail.com>
2016-03-20 19:31:30 +0100

    added charts

--- api/src/main/resources/static/js/controller.js
+++ api/src/main/resources/static/js/controller.js
@@ -1,4 +1,4 @@
-var app = angular.module('myApp', ['ngResource']);
+var app = angular.module('myApp', ['ngResource', 'chart.js']);
 
 app.factory('Entry', function($resource) {
     //return $resource('/hello?name=:name');
@@ -10,14 +10,26 @@ app.controller('MainCtrl', ['$scope', 'Entry',
     function ($scope, Entry) {
     $scope.name = "---";
     $scope.readings = [];
+    $scope.labels = [];
+    $scope.data = [[]];
+    var counter = 0;
 
     $scope.getGyro = function(){
         var resp = Entry.get({},
             function(){
                 $scope.readings.push(resp);
+                $scope.labels.push(counter++);
+                $scope.data[0].push(resp.accX);
             },
             function(){
                 $scope.name = "error";}
         );
     };
+
+    $scope.clear = function(){
+        $scope.readings = [];
+        $scope.labels = [];
+        $scope.data = [[]];
+        counter = 0;
+    }
 }]);
