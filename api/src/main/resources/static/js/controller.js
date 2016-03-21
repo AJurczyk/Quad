7dd8e29fc6a0853229dc83f82aab14cbe7efcb0d
AJurczyk <aleksander.jurczyk@gmail.com>
2016-03-21 14:15:08 +0100

    added n3charts

--- api/src/main/resources/static/js/controller.js
+++ api/src/main/resources/static/js/controller.js
@@ -1,4 +1,4 @@
-var app = angular.module('myApp', ['ngResource', 'chart.js']);
+var app = angular.module('myApp', ['ngResource', 'n3-line-chart']);
 
 app.factory('Entry', function($resource) {
     //return $resource('/hello?name=:name');
@@ -10,26 +10,46 @@ app.controller('MainCtrl', ['$scope', 'Entry',
     function ($scope, Entry) {
     $scope.name = "---";
     $scope.readings = [];
-    $scope.labels = [];
-    $scope.data = [[]];
     var counter = 0;
 
     $scope.getGyro = function(){
         var resp = Entry.get({},
             function(){
                 $scope.readings.push(resp);
-                $scope.labels.push(counter++);
-                $scope.data[0].push(resp.accX);
+                $scope.data.dataset0.push({x: counter, val_0: resp.accX});
+                counter+=1;
             },
             function(){
                 $scope.name = "error";}
         );
     };
 
+    $scope.data = {
+        dataset0: [
+        ]};
+
+    $scope.options = {
+      series: [
+        {
+          axis: "y",
+          dataset: "dataset0",
+          key: "val_0",
+          label: "An area series",
+          color: "#1f77b4",
+          type: ['line'],
+          id: 'mySeries0'
+        }
+      ],
+      axes: {
+        x: {
+            key: "x"
+        }
+      }
+    };
     $scope.clear = function(){
-        $scope.readings = [];
-        $scope.labels = [];
-        $scope.data = [[]];
         counter = 0;
-    }
+        $scope.data.dataset0=[];
+        $scope.readings = [];
+    };
+
 }]);
