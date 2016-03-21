d5ef53f1182ffef07d3d2bf200c4de970b34acf3
AJurczyk <aleksander.jurczyk@gmail.com>
2016-03-21 22:15:47 +0100

    chart updated

--- api/src/main/resources/static/js/controller.js
+++ api/src/main/resources/static/js/controller.js
@@ -5,19 +5,22 @@ app.factory('Entry', function($resource) {
     return $resource('/gyro');
 });
 
-
 app.controller('MainCtrl', ['$scope', 'Entry',
     function ($scope, Entry) {
     $scope.name = "---";
     $scope.readings = [];
     var counter = 0;
-
+    chartSpan = 10;
     $scope.getGyro = function(){
         var resp = Entry.get({},
             function(){
                 $scope.readings.push(resp);
-                $scope.data.dataset0.push({x: counter, val_0: resp.accX});
-                counter+=1;
+                $scope.data.dataset0.push({x: counter, accX: resp.accX, accY: resp.accY, accZ: resp.accZ});
+                if(counter > chartSpan){
+                    $scope.options.axes.x.min++;
+                    $scope.options.axes.x.max++;
+                }
+                counter++;
             },
             function(){
                 $scope.name = "error";}
@@ -28,28 +31,64 @@ app.controller('MainCtrl', ['$scope', 'Entry',
         dataset0: [
         ]};
 
+    $scope.addFakeData = function(){
+        $scope.data.dataset0.push({x: counter, accX: counter%5, accY: counter%5+1, accZ: counter%5+2});
+        if(counter>chartSpan){
+            $scope.options.axes.x.min++;
+            $scope.options.axes.x.max++;
+        }
+        counter++;
+    }
+
     $scope.options = {
       series: [
         {
           axis: "y",
           dataset: "dataset0",
-          key: "val_0",
-          label: "An area series",
-          color: "#1f77b4",
+          key: "accX",
+          label: "X",
+          color: "#FF0000",
           type: ['line'],
-          id: 'mySeries0'
+          id: 'seriesAccX'
+        },
+        {
+          axis: "y",
+          dataset: "dataset0",
+          key: "accY",
+          label: "Y",
+          color: "#00FF00",
+          type: ['line'],
+          id: 'seriesAccY'
+         },
+        {
+          axis: "y",
+          dataset: "dataset0",
+          key: "accZ",
+          label: "Z",
+          color: "#0000FF",
+          type: ['line'],
+          id: 'seriesAccZ'
         }
       ],
       axes: {
         x: {
-            key: "x"
+            key: "x",
+            min: 0,
+            max: chartSpan
         }
+      },
+      pan: {
+            x: true,
+            y: false
       }
     };
+
     $scope.clear = function(){
         counter = 0;
         $scope.data.dataset0=[];
         $scope.readings = [];
+        $scope.options.axes.x.min=0;
+        $scope.options.axes.x.max=chartSpan;
     };
 
 }]);
